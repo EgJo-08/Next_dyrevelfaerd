246578324844d84bcbce666ecde446fc6549992c
EgJo-08 <U2535495@rts-365.dk>
2026-06-18 11:22:48 +0200

    har fået fetchet og lavet css til admin/dashboard skal til at lave en virkenede slet knap og så en tilføj flere knap

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,20 +1,46 @@
 import { redirect } from "next/navigation";
 import { cookies } from "next/headers";
+import GetAnimals from "../../adopter/fetch";
+import "./dashboard.css";
 
-export default async function DashboardPage(){
+export default async function DashboardPage() {
+    const cookieStore = await cookies();
 
-    const cookieStore = await cookies()
+    if (!cookieStore.has("token")) {
+        redirect("/admin");
+    }
 
-    const token = false
+    const animals = await GetAnimals();
 
-    if(!cookieStore.has("token")) redirect('/admin')
+    return (
+        <section className="dashboard">
+            <h2 className="dashboard-title">Admin</h2>
 
+         <div className="animals-list">
+    {animals?.map((animal) => (
+        <article key={animal.id} className="animal-card">
+            <img
+                src={animal.asset.url}
+                alt={animal.name}
+                className="animal-image"
+            />
 
+            <div className="animal-info">
+                <h3 className="animal-name">{animal.name}</h3>
 
+                <div className="animal-actions">
+                    <button className="edit-btn">
+                        Rediger
+                    </button>
 
-    return(
-        <>
-        <h2>Her kommer dyr</h2>
-        </>
-    )
+                    <button className="delete-btn">
+                        Slet
+                    </button>
+                </div>
+            </div>
+        </article>
+    ))}
+</div>
+        </section>
+    );
 }
